c945fb2e7c3845a9d86d698c4489ada5f156afe9
oskar <oskar.michalskiii@gmail.com>
2018-10-30 19:10:37 +0100

    function add and remove user

--- src/main/java/LacksDataBaseProject/AddAndRemoveUser.java
+++ src/main/java/LacksDataBaseProject/AddAndRemoveUser.java
@@ -1,13 +1,44 @@
 package LacksDataBaseProject;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class AddAndRemoveUser {
-    public static void addUser(User user) {
-        User.listOfUsers.add(user);
+    static Scanner input = new Scanner(System.in);
+
+    public static void addUser(User user){
+        System.out.println("Name: ");
+        input = User.userName;
+        System.out.println("SkypeID: ");
+        input = User.skypeID;
+        System.out.println("Role: ");
+
+
+        System.out.println("Login: ");
+        input = User.login;
+        System.out.println("Password: ");
+        input = User.password;
+        System.out.println("Repeat password: ");
+        input = User.password2;
+        if (password.equals(password2)){
+            new User(userName, login, password, skypeIde, role);
+            break;
+        } else {
+            System.out.println("Password is not correct.");
+        }
+
     }
 
     public static void removeUser(User user) {
-
-        User.listOfUsers.remove(user);
+//        System.out.println("Give me user ID, to remove: ");
+//        for (User x : User.listOfUsers) {
+//            if (x.getUserID().) {
+//                User.listOfUsers.remove(user);
+//                System.out.println("User deleted.");
+//                return;
+//
+//            }
+//        }
     }
 }
 
